f9efd7d772626b6a7a014e9ae7796b285cf4a74a
amyotjl <jblai083@uottawa.ca>
2023-11-26 17:00:33 -0500

    training autoencoder and diffusion

---
+++ train_autoencoder1D_gridSearch.py
@@ -0,0 +1,109 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import joblib
+import os
+import gc
+import utils
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import EarlyStopping
+from sklearn.model_selection import KFold
+from torch.utils.data import DataLoader
+from Architectures.autoencoder_conv_pl import Autoencoder
+from pathlib import Path
+import argparse
+
+
+def run(args):
+    data = joblib.load(args.data)
+    data['sequences'] = np.array(data['sequences'])
+    random_matrices = joblib.load(args.rom_path)
+
+    Path(args.output_dir).mkdir(exist_ok=True)
+    config = {
+        'output_shape': [(180,22)],
+        'epochs': [3],
+        'bs':[args.batch_size],
+        'latent_dim':[args.latent_dim],
+        'channels':[[180,512,256],[180,512,64], [180, 512, 256, 128], [180,512,256,128,64]],
+        'conv_k': list(range(1,4)),
+        'conv_s': list(range(1,3)),
+        'conv_p': [0],
+        'activation_func': [nn.Sigmoid, nn.ReLU, nn.Tanh],
+        'loss_func': [nn.MSELoss(), nn.L1Loss()],
+        'optimizer' : [optim.SGD, optim.Adagrad, optim.AdamW],
+        'dropout_rate' : [0],
+        'batch_normalization' : [False]
+    }
+
+    kf = KFold(args.cross_validation)
+    used_model = []
+    if args.result_file: 
+        with open(args.result_file, 'r') as f:
+            parsed = f.read()
+            used_model = parsed.split('\n')
+
+
+    torch.set_float32_matmul_precision('medium')
+    for conf in utils.get_combinations(config):
+            gc.collect()
+            # Build the file name
+            file_name = '{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}'.format(
+                                    conf['latent_dim'], conf["channels"], conf['bs'], conf["activation_func"].__name__,
+                                    conf["optimizer"].__name__, conf["loss_func"].__class__.__name__, conf["dropout_rate"], conf["batch_normalization"],
+                                    conf['conv_k'],conf['conv_s'],conf['conv_p']
+                                    )
+            
+            # Check if you already trained that combination of parameter
+            # Checks in the current output folder or in the results file
+            if (not any(file_name in f for f in os.listdir(args.output_dir))) and (not any(file_name in m for m in used_model)):
+                print(conf)
+                try:
+                    #Performs Cross validation
+                    validation_accuracy = []
+                    for i, (train_index, test_index) in enumerate(kf.split(data['inputs'], data['sequences'])):
+                            
+                            #Create the dataloader
+                            train_loader = DataLoader(list(zip(data['inputs'][train_index], data['sequences'][train_index])), batch_size =  conf['bs'], shuffle=True, num_workers=2, persistent_workers=True, pin_memory=True)
+                            test_loader = DataLoader(list(zip(data['inputs'][test_index], data['sequences'][test_index])), batch_size =  conf['bs'], shuffle=False, num_workers=2, persistent_workers=True, pin_memory=True)
+                            
+                            #Create model
+                            model = Autoencoder(conf['output_shape'], conf['channels'], conf['latent_dim'], conv_k=conf['conv_k'],conv_s=conf['conv_s'],conv_p=conf['conv_p'], optimizer=conf['optimizer'], dropout_rate=conf['dropout_rate'], activation_function=conf['activation_func'], loss_func=conf['loss_func'], random_matrices_orth=random_matrices)
+
+                            # define Early stopping callback
+                            early_stopping = EarlyStopping(monitor="validation_epoch_loss", patience=3, min_delta=0.0001, verbose=True)
+
+                            #Create the Trainer from PyTorch Litghning. 
+                            trainer = pl.Trainer(devices=1, max_epochs=conf['epochs'], callbacks=[early_stopping], num_sanity_val_steps=0, check_val_every_n_epoch=1, enable_progress_bar=False)
+                            tuner = pl.tuner.Tuner(trainer)
+
+                            # Finds the best learning rate and updates the model's learning rate (lr) in place. No need to manually change it
+                            lr_finder = tuner.lr_find(model,train_dataloaders = train_loader, val_dataloaders=test_loader, update_attr=True)
+                            
+                            #trains the model
+                            trainer.fit(model, train_loader, test_loader)
+                            validation_accuracy.append(model.validation_acc[-1])
+                            #Save the model
+                            # joblib.dump(model, '{}/{}_{}'.format(args.output_dir, file_name,i)) # UNCOMMENT TO SAVE THE MODEL
+
+                            # Save only the last epoch validation accuracy.
+                            with open('{}/{}.result'.format(args.output_dir, file_name), 'w') as file: 
+                                file.write('{} | {}'.format(np.mean(validation_accuracy), validation_accuracy))
+                                
+                except :
+                    #ignore the failed model
+                    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-romp','--rom_path', type=str, help='path to the random orthogonal matrix stored in a JOBLIB file', required=True)
+    parser.add_argument('-data', type=str, help="Path to the Joblib file containing the data formated in \{inputs, sequences\} (The result of script create_orth_dataset.py)", required=True)
+    parser.add_argument('-cv', '--cross_validation', type=int, help='Number of folds for cross validation', default=5, required=False)
+    parser.add_argument('-out', '--output_dir', type=str, help='Folder where to save the models', required=True)
+    parser.add_argument('-res', '--result_file', type=str, help='Path to the file containing the results of previous experiment. It should be the file produce by "evalutate.py"',required=False, default=None)
+    parser.add_argument('-bs', '--batch_size', type=int, help='Batch size')
+    parser.add_argument('-ld', '--latent_dim', type=int, help='Latent dimension')
+
+    args = parser.parse_args()
+    run(args)
---
+++ train_diffusion1D.py
@@ -0,0 +1,165 @@
+import torch
+from torch.utils.data import Dataset
+from torch import Tensor
+from denoising_diffusion_1D import Trainer1D, Unet1D, GaussianDiffusion1D
+import joblib
+import glob
+import os
+import re
+import json
+import argparse
+import hashlib
+from utils import get_combinations, hash_dictionary
+
+
+def hash_dictionary(dictionary):
+    # Convert the dictionary to a JSON string
+    json_str = json.dumps(dictionary, sort_keys=True)
+
+    # Create a hash object using the SHA256 algorithm
+    hash_object = hashlib.sha256(json_str.encode())
+
+    # Get the hexadecimal representation of the hash
+    hash_hex = hash_object.hexdigest()
+
+    return hash_hex
+
+
+class Dataset1D(Dataset):
+    def __init__(self, tensor: Tensor):
+        super().__init__()
+        self.tensor = tensor.clone()
+
+    def __len__(self):
+        return len(self.tensor)
+
+    def __getitem__(self, idx):
+        return self.tensor[idx].clone()
+
+
+#Returns the dictionary without specified keys
+def without_keys(d, keys):
+    return {k: d[k] for k in d.keys() - keys}
+
+def train(config, data, args):
+    # creating Dataset1D
+    dataset_train = Dataset1D(data['inputs'].detach())
+
+    #Hashing the config and creating the folder
+    conf_to_hash_unet = without_keys(config['unet'], [])
+    conf_to_hash_gaussian = without_keys(config['gaussian'], [])
+    conf_to_hash_trainer = without_keys(config['trainer'], ['train_batch_size', 'train_num_steps', 'save_and_sample_every', 'num_samples'])
+
+    folder_name ='{}/{}'.format(args.output_dir,hash_dictionary( {**conf_to_hash_unet, **conf_to_hash_gaussian,**conf_to_hash_trainer}))
+
+
+    #Create the result folder for the current configuration
+    try:
+        os.makedirs(folder_name, exist_ok=False)
+    except Exception:
+    
+        print('ALREADY TRAINED ')
+        return
+
+
+    # Dump the configuration into a JSON file
+    with open('{}/config.json'.format(folder_name), 'w') as file:
+        json.dump(config, file, indent=4)
+
+    print(folder_name)
+
+
+    #creating diffusion model
+    model = Unet1D(
+        **config['unet']
+    ).cuda()
+
+    diffusion = GaussianDiffusion1D(
+        model,
+        **config['gaussian']
+    ).cuda()
+
+    trainer = Trainer1D(
+        diffusion,
+        dataset = dataset_train,
+        results_folder=folder_name,
+        **config['trainer']
+    )
+
+
+    # If the training was interupted, we load the last checkpoint
+    list_of_files = glob.glob('{}/*.pt'.format(folder_name)) # * means all if need specific format then *.csv
+    if len(list_of_files)>0:
+        latest_file = max(list_of_files, key=os.path.getctime)
+        print(latest_file)
+        pattern = r"-(.*?)\."
+        match = re.search(pattern, latest_file)
+        if match:
+            result = match.group(1)
+            print('Loading checkpoint: ', result)
+            trainer.load(result)
+   
+    trainer.train()
+
+#returns all combination of a given dictionary where values are lists
+def get_diffusion_combinations(config):
+    temp = {}
+    for key in config.keys():
+        temp[key] = get_combinations(config[key])
+    return get_combinations(temp)
+
+
+if __name__ == "__main__":    
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-data', type=str, help="Path to the Joblib file containing the LATENT data formated in \{inputs, sequences\} (The result of script create_orth_dataset.py)", required=True)
+    parser.add_argument('-out', '--output_dir', type=str, help='Folder where to save the models', required=True)
+    parser.add_argument('-d', '--dim', type=int, help='inital dimension of the model', default=16)
+
+    args = parser.parse_args()
+
+
+    conf = {
+    'trainer':{
+        'train_batch_size' : 128,
+        'gradient_accumulate_every' : 2,
+        'train_lr' : [0.001, 0.00001],
+        'train_num_steps' : 10000,
+        'ema_update_every' : 10,
+        'ema_decay' : 0.995,
+        'adam_betas' : (0.9, 0.99),
+        'save_and_sample_every' : 2000,
+        'num_samples' : 1, # must be square
+        'amp' : False,
+        'fp16' : False,
+        'split_batches' : True,
+    },
+    'unet':{
+        'dim':int(args.dim),
+        'dim_mults':[(1, 2, 4), (1, 2, 4, 8), (1, 2, 4, 8, 16)],
+        'channels':1,
+        'out_dim' : None,
+        'self_condition' : [True, False],
+        'resnet_block_groups' : [2,4,8],
+        'learned_variance' : False,
+        'learned_sinusoidal_cond' : False,
+        'random_fourier_features' : False,
+        'learned_sinusoidal_dim' : [8, 16]
+    },
+    'gaussian':{
+        'seq_length': 1024,
+        'timesteps' : 1000,
+        'sampling_timesteps' : None,
+        'loss_type' : ['l2','l1'],
+        'objective' : ['pred_x0'],
+        'beta_schedule' : ['cosine', 'linear'],
+        'ddim_sampling_eta' : 0.,
+        'auto_normalize' : True
+    },
+
+}
+
+    dataset = joblib.load(args.data)
+    for config in get_diffusion_combinations(conf):
+        print(config)
+        train(config, dataset, args)
